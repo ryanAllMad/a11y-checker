4f6e46b2c87bf15d051efe42fb7397695eea5ee4
Ryan Duer <ryan@allmaddesigns.com>
2024-03-25 07:25:31 -0700

    init

---
+++ a11y-check.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     A11y Check
+ * Plugin URI:      TBD
+ * Description:     Make your content accessible to more audiences. Use a11y check to check your posts for common accessibility pitfalls.
+ * Author:          Ryan Duer
+ * Author URI:      allmaddesigns.com
+ * Text Domain:     a11y-check
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         A11y_Check
+ */
+
+
